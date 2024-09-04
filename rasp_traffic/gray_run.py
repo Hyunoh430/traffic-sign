91a6e9bda35f40287685567c1586feb0ee120206
현오홍 <harrymickey@naver.com>
2024-09-04 19:47:15 +0900

    gray_run.py 출력 128*128로 수정, 모델 추론 시간 측정 추가

--- rasp_traffic/gray_run.py
+++ rasp_traffic/gray_run.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import torchvision.transforms as T
+import time
 
 def inference_and_save(model, input_image_path, output_image_path, threshold=0.3, class_names=None):
     # 이미지 로드 및 전처리
@@ -12,33 +13,37 @@ def inference_and_save(model, input_image_path, output_image_path, threshold=0.3
     img = img.resize((128, 128))  # 이미지 크기 조정
     img_tensor = T.ToTensor()(img).unsqueeze(0)  # 이미지 텐서로 변환 및 배치 차원 추가
 
+    # 모델 추론 시간 측정 시작
+    start_time = time.time()
+
     # 모델 추론
     with torch.no_grad():
         predictions = model(img_tensor.to(torch.device('cpu')))  # CPU로 모델 실행
 
+    # 추론 시간 계산
+    inference_time = time.time() - start_time
+    print(f"Inference time: {inference_time:.4f} seconds")
+
     # 결과 시각화
-    fig, ax = plt.subplots(1, figsize=(10, 10))
+    fig, ax = plt.subplots(1, figsize=(1.28, 1.28), dpi=100)  # 128x128 픽셀로 설정
     img_np = np.array(img)
     ax.imshow(img_np, cmap='gray')
 
     for i, (box, score, label) in enumerate(zip(predictions[0]['boxes'], predictions[0]['scores'], predictions[0]['labels'])):
         if score > threshold:
             x1, y1, x2, y2 = map(int, box)
-            rect = plt.Rectangle((x1, y1), x2-x1, y2-y1, fill=False, edgecolor='red', linewidth=2)
+            rect = plt.Rectangle((x1, y1), x2-x1, y2-y1, fill=False, edgecolor='red', linewidth=1)
             ax.add_patch(rect)
-            
             if class_names:
                 class_name = class_names[label.item()]
             else:
                 class_name = f"Class {label.item()}"
-            
-            ax.text(x1, y1, f'{class_name}: {score:.2f}', bbox=dict(facecolor='white', alpha=0.5))
+            ax.text(x1, y1, f'{class_name}', fontsize=4, bbox=dict(facecolor='white', alpha=0.5, linewidth=0))
 
     ax.axis('off')
-    plt.tight_layout()
-    plt.savefig(output_image_path)
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)  # 여백 제거
+    plt.savefig(output_image_path, dpi=100, bbox_inches='tight', pad_inches=0)
     plt.close()
-
     print(f'Result saved at {output_image_path}')
 
 # 사용 예시
